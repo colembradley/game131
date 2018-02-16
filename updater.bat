3723f2b7782a3960cb7e414c9037a87fe341b19b
svc-student <svc-student@student.directory.jccc>
2018-02-15 19:18:38 -0600

    deleted testing files and finished work

--- updater.bat
+++ updater.bat
@@ -1,9 +1,11 @@
-REM @ECHO OFF
+@ECHO OFF
 
 for /f %%a in ('git status -s') do set lines=%%a
 
 IF %lines% gtr 0 (
 	git pull --quiet
+	git merge -Xtheirs
+	echo Successful.
 ) ELSE (
 	echo You have uncommitted changes. You must commit or revert changes first.
 )
