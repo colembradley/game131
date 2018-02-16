8ed1a5c0480f2d0f27b6c6024a377dab8979d82d
svc-student <svc-student@student.directory.jccc>
2018-02-15 18:38:13 -0600

    test

--- reverter.bat
+++ reverter.bat
@@ -5,7 +5,7 @@ echo You will lose all changes since the last pull. Are you sure? y/n
 set /P confirmation= y/n:
 
 IF %confirmation%==y (
-	git reset --hard
+	git reset --hard --quiet
 	echo Changes have been reverted.
 ) ElSE (
 	echo Cancelled.
