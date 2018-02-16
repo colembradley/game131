0df98586842cafaf810ec9af00ebbcefbabfd8b5
svc-student <svc-student@student.directory.jccc>
2018-02-15 18:31:27 -0600

    test

--- committer.bat
+++ committer.bat
@@ -4,9 +4,9 @@ git add -A
 
 set /P message= Enter a message for this commit and push: 
 
-git commit -m "%message%" > NUL
+git commit -m "%message%" --quiet
 
-git push > NUL
+git push --quiet
 
 echo Successful.
 
