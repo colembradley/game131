98a95dcf77901ce323f5e458562c016a7e601b60
svc-student <svc-student@student.directory.jccc>
2018-02-15 19:14:51 -0600

    test

--- updater.bat
+++ updater.bat
@@ -1,8 +1,8 @@
-@ECHO OFF
+REM @ECHO OFF
 
 for /f %%a in ('git status -s') do set lines=%%a
 
-IF %lines% > 0 (
+IF %lines% gtr 0 (
 	git pull --quiet
 ) ELSE (
 	echo You have uncommitted changes. You must commit or revert changes first.
